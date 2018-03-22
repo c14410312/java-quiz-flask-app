c492deeabb31e3d5a4531b40c57f15f06b594174
Dylan Butler <dylan.butler@mydit.ie>
2018-03-22 13:42:57 +0000

    added category selection for quiz generation

--- app/routes.py
+++ app/routes.py
@@ -5,12 +5,21 @@ handlers for the application routes are written as PY functions --> view functio
 
 mapped to one or more route URLS --> flask knows the logic when to execute a client request
 '''
-from flask import render_template
+from flask import render_template, request
 from app import app
 from app.models import Post, Category
+import json
 
 @app.route('/') #decorators --> modifies function that follows it
 @app.route('/index')
 def index(): 
     categories = Category.query.all()
     return render_template('index.html', title='Home', categories=categories)
+
+
+#route for the quiz
+@app.route('/quiz')
+def quiz():
+    data = request.args.get('cat_list')
+    selected_cats = data.split(",")
+    return render_template('quiz.html', title='Quiz', categories=selected_cats)
--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,22 +1,41 @@
 //store the chosen categories
-let categories = []
+const categories = []
 
 
 $("document").ready(function(){
 
+	//function to deal with button clicks
 	$(".cat-button").click(function() {
 		
 		$(this).toggleClass('btn-default btn-success');
-		
-		value = $(this).text();
+	
+		//gets the value of the current button
+		value = $(this).text().trim();
 
+		//if the class of button --> success then push to array
 		if ($(this).hasClass( "btn-success" )){
 			categories.push(value);
-		}else{
+		}else{ //delete it from the array
 			categories = categories.filter(item => item !== value);
 		}
 		
-		console.log(categories);
+		//console.log(categories);
+	});
+
+	$("#gen-quiz").click(function() {
+		query = serialize({cat_list: categories});
+		console.log(query);
+		window.location.replace($SCRIPT_ROOT + '/quiz' + '?' + query);
 	});
 });
+
+const serialize = (obj) => {
+	let str = [];
+	for (let p in obj){
+		if (obj.hasOwnProperty(p)) {
+			 str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+		}
+		return str
+	}
+}
 
